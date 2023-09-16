03ace2339c7a8afbbcc525441e8d1564456c99eb
ARYAN DOGRA <dograwarrior2003@gmail.com>
2023-09-17 02:27:31 +0530

    grievance page done

--- client/src/pages/Grievance/Grievance.jsx
+++ client/src/pages/Grievance/Grievance.jsx
@@ -1,7 +1,137 @@
-import React from "react";
+import React, { useState } from "react";
+import classes from "./grievance.module.css";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
 
 function Grievance() {
-  return <>test</>;
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    mobileNumber: "",
+    emailAddress: "",
+    location: "",
+    pipeId: "",
+    comments: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log(formData);
+  };
+
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.container}>
+        <h1 style={{ fontSize: "32px", fontWeight: "bolder" }}>
+          File A Grievance
+        </h1>
+        <form onSubmit={handleSubmit} className={classes.form}>
+          <div className={classes.formGroup}>
+            <label htmlFor="firstName">First Name:</label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleInputChange}
+            />
+
+            <label htmlFor="lastName">Last Name:</label>
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleInputChange}
+            />
+          </div>
+          {/* <div className={classes.formGroup}></div> */}
+          <div className={classes.formGroup}>
+            <label htmlFor="mobileNumber">Mobile Number:</label>
+            <input
+              type="tel"
+              id="mobileNumber"
+              name="mobileNumber"
+              value={formData.mobileNumber}
+              onChange={handleInputChange}
+            />
+
+            <label htmlFor="emailAddress">Email Address:</label>
+            <input
+              type="email"
+              id="emailAddress"
+              name="emailAddress"
+              value={formData.emailAddress}
+              onChange={handleInputChange}
+            />
+          </div>
+          {/* <div className={classes.formGroup}></div> */}
+          <div className={classes.formGroup}>
+            <label htmlFor="location">Location:</label>
+            <input
+              type="text"
+              id="location"
+              name="location"
+              value={formData.location}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div></div>
+          <div className={classes.formGroup}>
+            <label htmlFor="comments">Comments:</label>
+            <textarea
+              id="comments"
+              name="comments"
+              value={formData.comments}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className={classes.formGroup}>
+            <Button
+              component="label"
+              variant="contained"
+              startIcon={<CloudUploadIcon />}
+              style={{
+                background: "#D3DFEC",
+                color: "#0d1e3f",
+                width: "28rem",
+                boxShadow: " 0 1px 6px black",
+                outlineStyle: "dotted;",
+              }}
+            >
+              Upload Images / Videos
+              <VisuallyHiddenInput type="file" />
+            </Button>
+          </div>
+        </form>
+        <button type="submit" onClick={handleSubmit}>
+          Submit
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Grievance;
